19b11e93d8eb9faa4160d7c4b813ab6092357e69
isagawa mito <isagawa1125@gmail.com>
2025-05-16 13:52:14 +0900

    マイグレーションテーブルの作成

---
+++ src/database/migrations/2025_05_16_134238_create_item_images_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemImagesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('item_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained()->onDelete('cascade');
+            $table->string('image_path');
+            $table->boolean('is_main')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_images');
+    }
+}
